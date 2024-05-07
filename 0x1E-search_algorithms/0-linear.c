301f420ff0c13b6b15ab5d356255d9e05225fe4b
vale-hash <newlechim@gmail.com>
2024-05-07 13:52:00 +0100

    0-linear search added condittion to check value

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array)
+	if (array && value)
 	{
 		for (i = 0; i < size; i++)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
